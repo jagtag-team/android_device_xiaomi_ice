e21182fef666ffaba85b378937a0893fb8eeffc5
jagath <jjjagath9@gmail.com>
2026-05-16 22:09:16 +0530

    ice: use MTK boot control HAL

--- device.mk
+++ device.mk
@@ -7,8 +7,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
+    android.hardware.boot@1.2-mtkimpl \
     android.hardware.boot@1.2-service
 
 PRODUCT_PACKAGES += \
